0f2bbe163597da0f32f7ebb417aa50529e67a7ba
LarsSevenhuijsen <lars.sevenhuijsen@student.hogent.be>
2026-02-19 09:42:31 +0100

    Add GebruikerController to Main for testing

--- JAVA-app/src/main/java/gui/Main.java
+++ JAVA-app/src/main/java/gui/Main.java
@@ -1,8 +1,10 @@
 package gui;
 
+import domein.GebruikerController;
 import domein.SiteController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,13 +13,19 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         SiteController sc = new SiteController();
+        GebruikerController gc = new GebruikerController();
 
         // FXML Loader -> leest FXML (layout), JavaFX nodes maken (Tableview, Buttons,..), @FXML velden/methodes koppelen
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/SiteOverviewView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/GebruikerOverviewView.fxml"));
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/SiteOverviewView.fxml"));
         // controller injecteren
+
         loader.setControllerFactory(type -> {
             if (type == SiteOverviewController.class)
                 return new SiteOverviewController(sc); // deze ipv no-args constructor
+            else if (type == GebruikerOverviewController.class)
+                return new GebruikerOverviewController(gc);
+
             try {
                 return type.getDeclaredConstructor().newInstance();
             } catch (Exception e) {
@@ -25,9 +33,21 @@ public class Main extends Application {
             }
         });
 
-        Scene scene = new Scene(loader.load());
+        Parent root = loader.load();
+        Object controller = loader.getController();
+
+        String title;
+        if (controller instanceof SiteOverviewController) {
+            title = "Sites beheren";
+        } else if (controller instanceof  GebruikerOverviewController) {
+            title = "Gebruikers beheren";
+        } else {
+            title = "Default";
+        }
+
+        Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Sites beheren");
+        stage.setTitle(title);
         stage.show();
     }
 
