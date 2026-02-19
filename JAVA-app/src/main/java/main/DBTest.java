5b76a7ef17eb1b5d9fb1e215229dbc66d513a704
LarsSevenhuijsen <lars.sevenhuijsen@student.hogent.be>
2026-02-19 09:39:35 +0100

    Fix Gebruiker creation

--- JAVA-app/src/main/java/main/DBTest.java
+++ JAVA-app/src/main/java/main/DBTest.java
@@ -2,11 +2,13 @@ package main;
 
 import domein.Gebruiker;
 import jakarta.persistence.EntityManager;
+import util.GebruikerStatus;
 import util.JPAUtil;
+import util.Rollen;
 
 public class DBTest {
     public static void main(String args[]) {
-        Gebruiker test = new Gebruiker("test","test");
+        Gebruiker test = Gebruiker.builder().email("test").gebruikersnaam("test").wachtwoord("test").status(GebruikerStatus.ACTIEF).rol(Rollen.WERKNEMER).build();
 
         try( EntityManager em
                      = JPAUtil.getENTITY_MANAGER_FACTORY().createEntityManager())
