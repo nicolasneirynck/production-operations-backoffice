c3a2455862b3cced03c5df56035e58cd21fc6637
LarsSevenhuijsen <lars.sevenhuijsen@student.hogent.be>
2026-02-24 16:13:34 +0100

    Fix tests (using new Gebruiker fields)

--- JAVA-app/src/test/java/GebruikerTest.java
+++ JAVA-app/src/test/java/GebruikerTest.java
@@ -12,26 +12,37 @@ import util.Rollen;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+// TODO: more tests
 public class GebruikerTest {
     private Gebruiker.Builder gebruikerBuilder;
 
     @BeforeEach
     void setUp() {
         gebruikerBuilder = Gebruiker.builder()
+                .personeelsnummer(1)
+                .naam("eenNaam")
+                .voornaam("eenVoornaam")
+                .geboortedatum("02/24/2000")
+                .adres("Straat 5; Stad; Land")
                 .email("geldigeEmail@gmail.com")
-                .gebruikersnaam("geldigeNaam")
-                .wachtwoord("geldigWachtwoord123")
+                .gsm("+32 2 152 45 62")
+                .rol(Rollen.WERKNEMER)
                 .status(GebruikerStatus.ACTIEF)
-                .rol(Rollen.WERKNEMER);
+                .wachtwoord("geldigWachtwoord123");
     }
 
     @ParameterizedTest
-    @CsvSource({"lars@gmail.com, Lars, mijnWachtwoord", "eenEmail@proton.me, Pérsöon, w@132_"})
-    void builder_GeldigeCombinaties_MaaktNieuweGebruikerMetCorrecteInfo(String email, String gebruikersnaam, String wachtwoord) {
-        Gebruiker gebruiker = gebruikerBuilder.email(email).gebruikersnaam(gebruikersnaam).wachtwoord(wachtwoord).build();
+    @CsvSource({"5, Achternaam, Lars, 08/02/1980, EenStraat 16; Gent; België, lars@gmail.com, +32 5 113 45 62, mijnWachtwoord", "512390581, Langelangelangelangelangeachternaam, Pérsöon, 12/12/1950, LangeLangeLangeStraat 16; Brussel; België, eenEmail@proton.me, +50 5 144 45 62, w@132_"})
+    void builder_GeldigeCombinaties_MaaktNieuweGebruikerMetCorrecteInfo(int personeelsnummer, String naam, String voornaam, String geboortedatum, String adres, String email, String gsm, String wachtwoord) {
+        Gebruiker gebruiker = gebruikerBuilder.personeelsnummer(personeelsnummer).naam(naam).voornaam(voornaam).geboortedatum(geboortedatum).adres(adres).email(email).gsm(gsm).wachtwoord(wachtwoord).build();
 
+        assertEquals(personeelsnummer, gebruiker.getPersoneelsnummer());
+        assertEquals(naam, gebruiker.getNaam());
+        assertEquals(voornaam, gebruiker.getVoornaam());
+        assertEquals(geboortedatum, gebruiker.getGeboortedatum());
+        assertEquals(adres, gebruiker.getAdres());
         assertEquals(email, gebruiker.getEmail());
-        assertEquals(gebruikersnaam, gebruiker.getGebruikersnaam());
+        assertEquals(gsm, gebruiker.getGsm());
         assertEquals(wachtwoord, gebruiker.getWachtwoord());
     }
 
@@ -54,14 +65,6 @@ public class GebruikerTest {
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {" ", "   "})
-    void builder_OngeldigeGebruikersnaam_GooitException(String gebruikersnaam) {
-        assertThrows(IllegalArgumentException.class, () -> gebruikerBuilder.gebruikersnaam(gebruikersnaam).build()
-        );
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    @ValueSource(strings = {" ", "   "})
     void builder_OngeldigWachtwoord_GooitException(String wachtwoord) {
         assertThrows(IllegalArgumentException.class, () -> gebruikerBuilder.wachtwoord(wachtwoord).build()
         );
@@ -69,7 +72,7 @@ public class GebruikerTest {
 
     @Test
     void builder_OngeldigeStatus_GooitException() {
-        assertThrows(IllegalArgumentException.class, () -> gebruikerBuilder.status(GebruikerStatus.VERWIJDERD).build()
+        assertThrows(IllegalArgumentException.class, () -> gebruikerBuilder.status(GebruikerStatus.INACTIEF).build()
         );
     }
 }
